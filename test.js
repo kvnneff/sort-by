bcfd925362402ab19e0e7021fa02e084e4bf69f6
River Grimm <river.grimm@gmail.com>
2014-09-27 14:50:35 -0700

    make tests easier to read

--- test.js
+++ test.js
@@ -1,38 +1,58 @@
-var sortBy = require('./index.js'),
-    assert = require('chai').assert,
-    array, array2;
+var sortBy = require('./index.js');
+var assert = require('component/assert');
+var array;
 
 array = [
-    {x: 4, y: 1},
-    {x: 3, y: 1},
-    {x: 2, y: 3},
-    {x: 1, y: 2}
-];
-
-array2 = [
     {x: 4, y: 1, z: {a: 2}},
     {x: 3, y: 1, z: {a: 3}},
     {x: 2, y: 3, z: {a: 3}},
     {x: 1, y: 2, z: {a: 1}}
 ];
-describe('sort', function () {
-    it('should sort an array of objects', function () {
+
+describe('Sort()', function () {
+    it('returns a function', function () {
+        assert(typeof sortBy === 'function');
+    });
+});
+
+describe('Sort(prop)', function () {
+    it('sorts an array of objects using given property name', function () {
+        array.sort(sortBy('x'));
+        assert(array[0] === {x: 1, y: 2, z: {a: 1}});
+        assert(array[3] === {x: 4, y: 1, z: {a: 2}});
+    });
+});
+
+describe('Sort(prop, prop)', function () {
+    it('sorts an array of objects using multiple property names', function () {
         array.sort(sortBy('y', 'x'));
-        assert.deepEqual(array, [{x: 3, y: 1}, {x: 4, y: 1}, {x: 1, y: 2}, {x: 2, y: 3}]);
+        assert(array[0] === {x: 3, y: 1, z: {a: 3}});
+        assert(array[3] === {x: 2, y: 3, z: {a: 3}});
     });
+});
 
-    it('should reverse the order', function () {
-        array.sort(sortBy('x', '-y'));
-        assert.deepEqual(array, [{x: 1, y: 2}, {x: 2, y: 3}, {x:3, y: 1}, {x: 4, y: 1}]);
+describe('Sort(-prop)', function () {
+    it('reverses the sort order', function () {
+        array.sort(sortBy('-y', 'x'));
+        assert(array[0] === {x: 2, y: 3, z: {a: 3}});
+        assert(array[3] === {x: 4, y: 1, z: {a: 2}});
     });
+});
 
-    it('should sort an array of objects with nested properties', function () {
-        array2.sort(sortBy('z.a', 'y'));
-        assert.deepEqual(array2, [{x: 1, y: 2, z: {a: 1}}, {x: 4, y: 1, z: {a: 2}}, {x: 3, y: 1, z: {a: 3}}, {x: 2, y: 3, z: {a: 3}}]);
+describe('Sort(prop.prop)', function () {
+    it('sorts an array of objects with nested properties', function () {
+        array.sort(sortBy('z.a', 'y'));
+        assert(array[0] === {x: 1, y: 2, z: {a: 1}});
+        assert(array[3] === {x: 2, y: 3, z: {a: 3}});
     });
+});
 
-    it('should reverse sort an array of objects with nested properties', function () {
-        array2.sort(sortBy('-z.a', 'y'));
-        assert.deepEqual(array2, [{x: 3, y: 1, z: {a: 3}}, {x: 2, y: 3, z: {a: 3}}, {x: 4, y: 1, z: {a: 2}}, {x: 1, y: 2, z: {a: 1}}]);
+describe('Sort(-prop.prop', function () {
+    it('reverse sorts an array of objects with nested properties', function () {
+        array.sort(sortBy('-z.a', 'y'));
+        assert(array[0] === {x: 3, y: 1, z: {a: 3}});
+        assert(array[1] === {x: 2, y: 3, z: {a: 3}});
+        assert(array[2] === {x: 4, y: 1, z: {a: 2}});
+        assert(array[3] === {x: 1, y: 2, z: {a: 1}});
     });
 });
